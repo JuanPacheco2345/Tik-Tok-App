9d800016759c61c4ab8a8472145926bae1e3cdc5
unknown <juanpacheco2345@gmail.com>
2021-01-19 13:42:42 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Video from "./Video";
+import db from "./firebase";
+
+function App() {
+    const [videos, setVideos] = useState([]);
+
+
+    useEffect(() => {
+
+
+
+        db.collection('videos').onSnapshot(snapshot => (
+            setVideos(snapshot.docs.map(doc => doc.data()))
+        ))
+
+    }, [])
+
+
+
+  return (
+    <div className="app">
+          <div className="app_videos">
+              {videos.map(
+                  ({ url, channel, description, song, likes, shares, messages }) => (
+
+                  
+
+                  <Video
+                        url = { url }
+                        channel = { channel }
+                        song = { song }
+                        likes={ likes }
+                         messages={ messages }
+                          description = { description }
+                          shares = {shares}
+
+
+                      />
+
+                 )
+          )}
+
+          </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+﻿import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdjLLRV0VoPF6FHNs_mA9zIUnxMo57ob4",
+    authDomain: "tik-tok-clone-94d16.firebaseapp.com",
+    databaseURL: "https://tik-tok-clone-94d16.firebaseio.com",
+    projectId: "tik-tok-clone-94d16",
+    storageBucket: "tik-tok-clone-94d16.appspot.com",
+    messagingSenderId: "906021573194",
+    appId: "1:906021573194:web:c1089808f0dccefeb38cb3",
+    measurementId: "G-WS0VX7RBMP",
+};
+
+const firebaseApp =
+    firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
